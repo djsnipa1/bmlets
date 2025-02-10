538a911b70b5d1173f76743dca39550ae7b3a1cc
Chad Boyce <chadboyce@gmail.com>
2025-02-09 22:44:48 -0500

    fixing reviews.js
    also fixed deploy in package.json

--- bookmarklets/src/reviews.js
+++ bookmarklets/src/reviews.js
@@ -1,7 +1,14 @@
-var url = document.URL;
-if (url.includes("amazon.com")) {
-    var finalUrl = `https://fakespot.com/analyze?ra=true&url=${url}`; 
-    window.location.href = finalUrl;
+// var url = document.URL;
+// if (url.includes("amazon.com")) {
+//     var finalUrl = `https://fakespot.com/analyze?ra=true&url=${url}`;
+//     window.location.href = finalUrl;
+// } else {
+//     alert("not an amazon product");
+// }
+
+var url = location.href;
+if (url.includes('amazon.com')) {
+	window.open('http://fakespot.com/analyze?url=' + encodeURIComponent(url), '_blank');
 } else {
-    alert("not an amazon product");
+	alert('not an amazon.com product page');
 }
